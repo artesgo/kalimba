c2e8fa0cec546cdb4168a5fb7efeeddde5213504
artesgo <artesgo@gmail.com>
2020-07-11 18:47:18 -0400

    setup dev environment

--- rollup.config.js
+++ rollup.config.js
@@ -3,69 +3,51 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import html from 'rollup-plugin-html2';
+import serve from 'rollup-plugin-serve';
+require('dotenv').config();
 
-const production = !process.env.ROLLUP_WATCH;
+const isDev = process.env.NODE_ENV === 'development';
+const port = 3000;
 
-export default {
+// define all our plugins
+const plugins = [
+	svelte({
+		dev: isDev,
+		extensions: ['.svelte'],
+	}),
+	resolve({
+		browser: true,
+		dedupe: ['svelte'],
+	}),
+	commonjs(),
+	// injects your bundles into index page
+	html({
+		template: 'src/index.html',
+		fileName: 'index.html',
+	}),
+];
+
+if (isDev) {
+	plugins.push(
+		// like a webpack-dev-server
+		serve({
+			contentBase: './dist',
+			historyApiFallback: true, // for SPAs
+			port,
+		}),
+		livereload({ watch: './dist' })
+	);
+} else {
+	plugins.push(terser());
+}
+
+module.exports = {
 	input: 'src/main.js',
 	output: {
-		sourcemap: true,
+		name: 'bundle',
+		file: 'dist/bundle.js',
 		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
 	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+	plugins,
 };
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
-}
